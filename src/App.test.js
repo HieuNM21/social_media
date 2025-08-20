bd616e2a48ef443170f2c4533f114fcca741f524
HieuNM21 <nmhieu21@gmail.com>
2025-08-20 15:59:22 +0700

    fix index.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders sidebar menu', () => {
-  render(<App />);
-  const homeLink = screen.getByText(/Trang chủ/i);
-  expect(homeLink).toBeInTheDocument();
-});
